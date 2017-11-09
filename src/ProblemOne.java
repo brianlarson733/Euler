b2cc7b548c500b5aba3aee4032b1780cfdd624ec
brianlarson733 <brian.larson@liferay.com>
2017-11-09 09:15:41 -0600

    Problem One complete

--- src/ProblemOne.java
+++ src/ProblemOne.java
@@ -3,36 +3,92 @@
 * get 3, 5, 6 and 9. The sum of these multiples is 23.
 *
 * Find the sum of all the multiples of 3 or 5 below 1000.
+* 
+* To improve - use try/catch for possible string input of numbers, refactor to have methods of 
+* reusable code.
 */
+
+import java.util.*;
 
 public class ProblemOne {
 	
 	public static void main(String[] args){
 		
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("How many numbers do you want to enter? Enter a positive " 
+						 + "whole number (e.g., 1, 5, 100, etc.).");
+
+		int numDivisors = Integer.parseInt(scanner.next());
+
+		while ((numDivisors < 0) | (numDivisors != (int) numDivisors)){
+			System.out.println("Sorry, that wasn't a positive whole number. "
+						   + "Please enter a positive whole number "
+						   + "(such as 4, 7, 15, etc.) to signify how many divisors "
+						   + "I should save.");
+			numDivisors = Integer.parseInt(scanner.next());
+		}
+
+		LinkedList<Integer> divisors = new LinkedList<Integer>(); // this list will hold the
+														  // divisors
+
+		for (int i = 1; i <= numDivisors; i++){
+
+			System.out.println("Please input a positive whole number as the divisor and "
+						   + "press the Enter/Return key.");
+			
+			int divisor = Integer.parseInt(scanner.next());
+			
+			while ((divisor < 0) | (divisor != (int) divisor)){
+				System.out.println("Sorry, that wasn't a positive whole number. "
+							   + "Please enter a positive whole number "
+							   + "(such as 4, 7, 15, etc.) as the divisor.");
+							   
+				divisor = Integer.parseInt(scanner.next());
+			}
+
+			divisors.add(divisor);
+		}
+
+		System.out.println("What positive number is the upper limit (non-inclusive) when "
+					   + "checking for multiples?");
+
+		int upperLimit = Integer.parseInt(scanner.next()); 
+
+		while ((upperLimit < 0) | (upperLimit != (int) upperLimit)){
+			System.out.println("Sorry, that wasn't a positive whole number. "
+						   + "Please enter a positive whole number "
+						   + "(such as 4, 7, 15, etc.) as the divisor.");
+						   
+			upperLimit = Integer.parseInt(scanner.next());
+		}
+
+		LinkedList<Integer> multiples = new LinkedList<Integer>(); // this list will hold the 
+														   // confirmed multiples.	
 		
+
+		for (int x = upperLimit - 1; x>0; x--){		
+			
+			int y;
+
+			for (y=0; (y < divisors.size()) && (x % divisors.get(y) != 0); y++){
+
+			}
+			
+			if (y != divisors.size()) {
+				
+				multiples.add(x); 
+			}
+		}		
 		
-	}
-	
-}
+		int sum = 0;
 
+		while (multiples.size() > 0){
+			
+			sum += multiples.poll();
 
-PSEUDO:
-
-list numbers = userInput1;
-int upperLimit = userInput2;
-list multiples
-int x = upperLimit - 1; 
-int y = 0; // used to iterate through numbers
-
-for (x; x > 0;){
-
-	while (x % numbers[y] != 0 & y < numbers.length)
-		y++;
-	if (y == numbers.length)
-		x--;
-	else 
-		multiples.add(x);
-
-
-
+		}
+		System.out.print("The sum is " + sum + ".");
+		
+	}	
 }
