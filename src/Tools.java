806227f0e0ff12bd4cf1e877be978afd8ef3c93b
brianlarson733 <brian.larson@liferay.com>
2017-11-13 14:56:00 -0600

    completed ProblemTwo, added reusable Tools class

---
+++ src/Tools.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Tools {
+
+	public static int sumLinkedList(LinkedList<Integer> list) {
+
+		int sum = 0;
+
+		while (list.size() > 0){
+			sum += list.poll();
+		}
+		return sum;
+	}
+
+
+	public static int getWholeNumberInput() {
+
+		Scanner scanner = new Scanner(System.in);
+
+		String placeholder = "";
+		int toReturn = -1;
+
+		while ((toReturn < 0) | (toReturn != (int) toReturn)){
+
+			System.out.println("Please enter a positive whole number.");			  
+			placeholder = scanner.nextLine();
+
+			try {
+
+				toReturn = Integer.parseInt(placeholder);
+		
+			}
+
+			catch (NumberFormatException e) {
+				System.out.print("Sorry, that wasn't a whole number. ");
+			}
+		}
+
+		return toReturn;
+	}
+
+}
